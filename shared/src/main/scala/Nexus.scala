7144d71e33ee87b86db2e6a9a141f739ca7dab2b
Neel Smith <nsmith@Technites.local>
2020-06-09 07:52:12 -0400

    Implement basic function of retrieving a block

--- shared/src/main/scala/Nexus.scala
+++ shared/src/main/scala/Nexus.scala
@@ -11,9 +11,52 @@ import scala.scalajs.js.annotation._
 
 @JSExportAll
 case class Nexus(nexusString: String) extends LogSupport {
-  val lines = nexusString.split("\n")
+
+  /** Nexus format is line-oriented, so work with
+  * data as a Vector of Strings.
+  */
+  val lines = nexusString.split("\n").toVector
+
   require(lines.head.toLowerCase.startsWith("#nexus"), "Invalid NEXUS format: required header line '#NEXUS' missing.")
 
+  def linesForBlock(blockLabel: String, includeComments: Boolean = false) : Vector[String] = {
+    linesForBlock(blockLabel, lines, Vector.empty[String], false, includeComments )
+
+  }
+
+  /** Extract lines contained within a named block.
+  */
+  def linesForBlock(blockLabel: String,
+    srcLines: Vector[String],
+    results: Vector[String],
+    inBlock: Boolean,
+    includeComments: Boolean): Vector[String] = {
+
+    val hdr = "begin " + blockLabel.toLowerCase + ";"
+    if (srcLines.isEmpty) {
+      results
+
+    } else if (inBlock && srcLines.head.trim.startsWith("end;")) {
+      results
+
+    } else {
+
+      if (srcLines.head.toLowerCase.trim == hdr) {
+        linesForBlock(blockLabel, srcLines.tail, results, true, includeComments)
+
+
+      } else {
+        if (inBlock) {
+          val newResults = results :+ srcLines.head
+          linesForBlock(blockLabel, srcLines.tail, newResults, inBlock, includeComments)
+
+        } else {
+          linesForBlock(blockLabel, srcLines.tail, results, inBlock, includeComments)
+        }
+      }
+
+    }
+  }
 }
 
 // See discussions at
--- shared/src/test/scala/NexusSpec.scala
+++ shared/src/test/scala/NexusSpec.scala
@@ -20,7 +20,26 @@ class NexusSpec extends FlatSpec {
       val nxs = Nexus(badSrc)
     }
   }
-  it should  "extract an artibtrarily named block" in pending
+  it should  "extract an artibtrarily named block" in {
+    val nexusString = """#nexus
+    begin taxa;
+      dimensions ntax=5;
+      taxlabels
+        Giardia
+        Thermus
+        Deinococcus
+        Sulfolobus
+        Haobacterium
+      ;
+    end;
+    more data here but we're already done."""
+    val taxa = Nexus(nexusString).linesForBlock("taxa")
+    val expectedLines = 8
+    assert(taxa.size == expectedLines)
+    val dims = "dimensions ntax=5;"
+    assert(taxa.head.trim == dims)
+    assert(taxa.last.trim == ";")
+  }
   it should  "extract lines for an arbitrarily named block" in pending
   it should "extract DATA lines" in pending
   it should "extract a DATA block" in pending
